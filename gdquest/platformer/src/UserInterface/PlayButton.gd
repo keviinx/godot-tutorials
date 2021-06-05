c0a991809b474d91d208b52154890164187839bc
keviinx <keviinx@yahoo.com>
2021-06-05 23:16:45 +0200

    done until coding the buttons

--- gdquest/platformer/src/UserInterface/PlayButton.gd
+++ gdquest/platformer/src/UserInterface/PlayButton.gd
@@ -1,6 +1,11 @@
+tool
 extends Button
 
-
+export(String, FILE) var next_scene_path := ""
 
 func _on_PlayButton_button_up():
-	get_tree().change_scene("res://src/Levels/Level01.tscn")
+	get_tree().change_scene(next_scene_path)
+
+
+func _get_configuration_warning():
+	return "next_scene_path must be set for the button to work" if next_scene_path == "" else ""
