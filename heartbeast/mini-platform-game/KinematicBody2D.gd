1534f261108f619021831a57cca80b0095a395dd
keviinx <keviinx@yahoo.com>
2021-06-13 12:46:12 +0200

    finished tutorial part 3 heartbeast platformer game

--- heartbeast/mini-platform-game/KinematicBody2D.gd
+++ heartbeast/mini-platform-game/KinematicBody2D.gd
@@ -2,7 +2,8 @@ extends KinematicBody2D
 
 const UP = Vector2(0, -1)
 const GRAVITY = 20
-const SPEED = 200
+const ACCELERATION = 50
+const MAX_SPEED = 200
 const JUMP_HEIGHT = -550
 
 # Declare member variables here. Examples:
@@ -16,24 +17,34 @@ var motion = Vector2.ZERO
 
 # Called during the physics processing step of the main loop.
 func _physics_process(delta):
+	var friction = true
+	
 	motion.y += GRAVITY
 	
 	if Input.is_action_pressed("ui_right"):
-		motion.x = SPEED
+		motion.x = min(motion.x + ACCELERATION, MAX_SPEED)
 		$Sprite.flip_h = false
 		$Sprite.play("Run")
 	elif Input.is_action_pressed("ui_left"):
-		motion.x = -SPEED
+		motion.x = max(motion.x - ACCELERATION, -MAX_SPEED)
 		$Sprite.flip_h = true
 		$Sprite.play("Run")
 	else:
 		$Sprite.play("Idle")
-		motion.x = 0
+		friction = true
 		
 	if is_on_floor():
 		if Input.is_action_just_pressed("ui_up"):
 			motion.y = JUMP_HEIGHT
+		if friction == true:
+			motion.x = lerp(motion.x, 0, 0.2)
 	else:
-		$Sprite.play("Jump")
+		# +y if jumping
+		if motion.y < 0:
+			$Sprite.play("Jump")
+		else:
+			$Sprite.play("Fall")
+		if friction == true:
+			motion.x = lerp(motion.x, 0, 0.05)
 		
 	motion = move_and_slide(motion, UP)
