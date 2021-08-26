25f065155cff345ac917272a6f19e562c1f408b6
Keviindran Ramachandran <keviinx@yahoo.com>
2021-08-26 22:21:49 +0200

    finished Moving the player

---
+++ clear-code/pong-godot/Player/Player.gd
@@ -0,0 +1,15 @@
+extends KinematicBody2D
+
+
+# Member variable
+var speed = 400
+
+
+# Called during the physics processing step of the main loop
+func _physics_process(delta):
+	var velocity = Vector2.ZERO
+	if Input.is_action_pressed("ui_up"):
+		velocity.y -= 1
+	if Input.is_action_pressed("ui_down"):
+		velocity.y += 1
+	move_and_slide(velocity * speed)
